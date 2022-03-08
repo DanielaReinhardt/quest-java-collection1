95e101c2ff2d23b0a6fd0d9c8fdc70eb157e795c
DanielaReinhardt <reinhardtd@telekom.de>
2022-03-08 15:52:29 +0100

    heros

--- Thanos.java
+++ Thanos.java
@@ -3,10 +3,49 @@ import java.util.*;
 public class Thanos {
 
     public static void main(String[] args) {
-
+        
         // TODO 1 : Create an empty heroes list
 
+        Hero widow = new Hero("Black Widow", 34);
+        Hero captain = new Hero("Captain America", 100);
+        Hero vision = new Hero("Vision", 3);
+        Hero iron = new Hero("Iron Man", 48);
+        Hero scarlet = new Hero("Scarlet Witch", 29);
+        Hero thor = new Hero("Thor", 1500);
+        Hero hulk = new Hero("Hulk", 49);
+        Hero doctor = new Hero("Doctor Strange", 42);
+
         // TODO 2 : Add those heroes to the list
+
+        ArrayList<Hero> heros = new ArrayList<>();        
+        heros.add(widow);
+        heros.add(captain);
+        heros.add(vision);
+        heros.add(iron);
+        heros.add(scarlet);
+        heros.add(thor);
+        heros.add(hulk);
+        heros.add(doctor);
+
+        thor.setAge(1501);
+
+        Collections.shuffle(heros);
+
+        heros.remove(0);
+        heros.remove(1);
+        heros.remove(2);
+        heros.remove(3);
+
+       
+
+        for (Hero people : heros) { 
+          System.out.println(people.getName() + " , " + people.getAge());
+    }
+
+        
+
+       
+
         // name: Black Widow, age: 34
         // name: Captain America, age: 100
         // name: Vision, age: 3
